3cda5592babdd65fb6e43c4ac4978b39e2cabfcf
GitFyHub <Younus@mcbp3621.local>
2017-03-06 00:56:29 -0600

    Updated sort function

--- insertion_sort.py
+++ insertion_sort.py
@@ -3,21 +3,18 @@ def sort(list):
     for index in range(1, len(list)):
         value = list[index]
         i = index - 1
-        while i >= 0:
-            if value < list[i]:
-                list[i + 1] = list[i]
-                list[i] = value
-                i = i - 1
-            else:
-                break
+        while i >= 0 and (value < list[i]):
+            list[i + 1] = list[i]
+            list[i] = value
+            i = i - 1
 
 
 def caller():
-    a = [13333, 2, 3, 42, 2676547135678567879896896, 45454, -9,2342342424234234]
+    a = [-1, 13333, 2, 3, 42, 2676547135678567879896896, 45454, -9,2342342424234234]
     sort(a)
     print(a)
 
 
-# - - - - - - - - - -
+# - - python console call - - -
 # from insertion_sort import *
 # caller()
