46bd57abff7a9867ed22e711516b144f3250a901
GitFyHub <Younus@mcbp3621.local>
2017-03-06 00:45:27 -0600

    Updated code with a caller function and comments

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,11 +1,23 @@
-def insertion_sort(list):
-    for index in range(1,len(list)):
-            value = list[index]
-            i = index - 1
-            while i >= 0:
-                if value < list[i]:
-                    list[i+1] = list[i]
-                    list[i] = value
-                    i = i - 1
-                else:
-                    break
+def sort(list):
+    # type: (object) -> object
+    for index in range(1, len(list)):
+        value = list[index]
+        i = index - 1
+        while i >= 0:
+            if value < list[i]:
+                list[i + 1] = list[i]
+                list[i] = value
+                i = i - 1
+            else:
+                break
+
+
+def caller():
+    a = [13333, 2, 3, 42, 2676547135678567879896896, 45454, -9,2342342424234234]
+    sort(a)
+    print(a)
+
+
+# - - - - - - - - - -
+# from insertion_sort import *
+# caller()
